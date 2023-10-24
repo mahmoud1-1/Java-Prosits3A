cc957bd92bb48afa220898275bd89716b26b28aa
mahmoud1-1 <mahmoud.dimassi@esprit.tn>
2023-10-24 21:43:24 +0100

    prosit2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+//-----------------------Prosit1-----------------------
+/*import java.util.Scanner;
 
 public class ZooManagement {
     // Déclaration des variables de classe
@@ -36,5 +37,40 @@ public class ZooManagement {
 
         // Appel de la méthode pour afficher les informations du zoo
         zoo.displayZooInfo();
+    }
+}*/
+//-----------------------Prosit2-----------------------
+
+public class ZooManagement {
+    public static void main(String[] args){
+        Animal lion = new Animal();
+        lion.name = "Simba";
+        lion.age = 8;
+        lion.family = "Cats";
+        lion.isMammal = true;
+
+        Zoo myZoo = new Zoo();
+        myZoo.name = "Wildlife Park";
+        myZoo.city = "Ariana";
+        myZoo.nbrCages = 25;
+        myZoo.animals = new Animal[25];
+
+
+        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+
+        myZoo.displayAnimals();
+
+        System.out.println(myZoo.searchAnimal(dog));
+        Animal dog2 = new Animal("Canine", "Snoopy", 2, true);
+        System.out.println(myZoo.searchAnimal(dog2));
+
+        System.out.println(myZoo.removeAnimal(dog));
+        myZoo.displayAnimals();
     }
 }
