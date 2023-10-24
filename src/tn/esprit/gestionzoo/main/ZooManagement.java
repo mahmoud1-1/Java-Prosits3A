0b53afcbebfcd718213b01a39ac702e4e3e2785b
mahmoud1-1 <mahmoud.dimassi@esprit.tn>
2023-10-24 22:59:02 +0100

    prosit6

--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -4,14 +4,11 @@ import tn.esprit.gestionzoo.entities.*;
 public class ZooManagement {
 
     public static void main(String[] args) {
-        Aquatic aquaticAnimal = new Aquatic("Fish", "Salmon", 2, false, "Water habitat");
-
-        Dolphin dolphin = new Dolphin("Ocean", 25.5f);
-        Penguin penguin = new Penguin("Antarctic", 10.0f);
-
-        System.out.println(aquaticAnimal);
-        System.out.println(dolphin);
-        System.out.println(penguin);
+        Aquatic bouri =new Aquatic("String family", "String name", 1, true, "String habitat");
+        Aquatic nazali =new Aquatic("String family", "String name", 1, true, "String habitat");
+        myZoo.addAquaticAnimal(bouri);
+        myZoo.addAquaticAnimal(nazali);
+        myZoo.afficherSwim();
 
 
     }
